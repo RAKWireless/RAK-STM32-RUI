694dd189d0b39487a5470476a1709d6f42859367
Eddie Ho <Eddie.Ho@rakwireless.com>
2022-05-09 13:51:53 +0000

    This is RUI3 SDK inside 3.4.0-rui3_22q1_final_update.99 release.

--- cores/STM32WLE/version.c
+++ cores/STM32WLE/version.c
@@ -1,9 +1,9 @@
-#define FW_REVISION "3.4.0-rui3_22q1_final_update.96"
+#define FW_REVISION "3.4.0-rui3_22q1_final_update.99"
 #define MODEL_ID "rak3172"
 #define CHIP_ID "stm32wle5xx"
-#define BUILD_DATE "20220508"
-#define BUILD_TIME "174658"
-#define REPO_INFO "f72a986d:fa6b6f5c:d821e1ff:ffbe0e20:68eca096:cffb263d:a297f955:d3b28c94"
+#define BUILD_DATE "20220509"
+#define BUILD_TIME "100557"
+#define REPO_INFO "938b8e59:fa6b6f5c:4ce3fe97:ffbe0e20:68eca096:17db53e6:a297f955:0b708b4d"
 #define CLI_REVISION "1.5.3"
 #define API_REVISION "3.1.1"
 const char *sw_version = {FW_REVISION};
