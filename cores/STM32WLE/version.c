98a7f509a910fd2dc5fa67b7fd9d9b73fe74264b
Eddie Ho <Eddie.Ho@rakwireless.com>
2022-05-22 21:25:34 +0800

    For RAK4630, use single bank DFU instead of dual bank DFU to prevent factory default & user data being erased.

--- cores/STM32WLE/version.c
+++ cores/STM32WLE/version.c
@@ -1,9 +1,9 @@
-#define FW_REVISION "3.4.2-rui3_22q1_update.111"
+#define FW_REVISION "3.4.2-rui3_22q1_update.112"
 #define MODEL_ID "rak3172"
 #define CHIP_ID "stm32wle5xx"
 #define BUILD_DATE "20220522"
-#define BUILD_TIME "073944"
-#define REPO_INFO "e998dd33:5f721a06:252b9dc0:4c985e34:b6fc40dd:033b119a:708d73e9:7da971ca"
+#define BUILD_TIME "110058"
+#define REPO_INFO "f4847cd5:5f721a06:252b9dc0:4c985e34:b6fc40dd:033b119a:708d73e9:7da971ca"
 #define CLI_REVISION "1.5.4"
 #define API_REVISION "3.2.0"
 const char *sw_version = {FW_REVISION};
