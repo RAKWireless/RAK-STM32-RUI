ffd18175fda8cb07039bb5afac642708cd2294c7
ian.lo <ian.lo@rakwireless.com>
2023-11-21 16:59:55 +0800

    This is RUI3 SDK inside RUI_4.1.0 release.

--- variants/WisDuo_RAK3172-T_Board/version.c
+++ variants/WisDuo_RAK3172-T_Board/version.c
@@ -1,9 +1,9 @@
 #define FW_REVISION "RUI_4.1.0_RAK3172-T"
 #define MODEL_ID "rak3172T"
 #define CHIP_ID "stm32wle5xx"
-#define BUILD_DATE "20231117"
-#define BUILD_TIME "041509"
-#define REPO_INFO "a8c9c7e8:ec0c4fa6:1f1d3237:4785713c:d65536ca:9a906d7c:c129743d:9d64089f:69e7409c:7b55ac52:6a3d6842:b9d39ab6:d5c6e88d:a29aaa6e:da54579d:d1e75214:1365fb0c:714946eb:a8c8127d:dcdae954"
+#define BUILD_DATE "20231121"
+#define BUILD_TIME "051825"
+#define REPO_INFO "eb6d4998:ec0c4fa6:1f1d3237:4785713c:d65536ca:9a906d7c:c129743d:9d64089f:69e7409c:7b55ac52:6a3d6842:b9d39ab6:d5c6e88d:a29aaa6e:da54579d:d1e75214:1365fb0c:714946eb:a8c8127d:dcdae954"
 #define CLI_REVISION "1.5.12"
 #define API_REVISION "3.2.8"
 const char *sw_version = {FW_REVISION};
