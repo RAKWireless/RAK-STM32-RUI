46b92ef823bc9c79d2bd6493fd397117d395716a
ian.lo <ian.lo@rakwireless.com>
2024-12-18 15:34:39 +0800

    Revert "This is RUI3 SDK inside RUI_5.0.1 release."
    This reverts commit 7e1f147d6778d037892938756ea20797b870e223.

--- variants/WisDuo_RAK3172-T_Board/version.c
+++ variants/WisDuo_RAK3172-T_Board/version.c
@@ -1,9 +1,9 @@
-#define FW_REVISION "RUI_5.0.1_RAK3172-T"
+#define FW_REVISION "RUI_5.0.0_RAK3172-T"
 #define MODEL_ID "rak3172T"
 #define CHIP_ID "stm32wle5xx"
-#define BUILD_DATE "20241216"
-#define BUILD_TIME "021128"
-#define REPO_INFO "7cd97386:2835831b:fb7108a4:08be607d:44d67462:df1bbf82:827629a7:44ca8327:1de43ba9:b7bed6b1:6a3d6842:b9d39ab6:d5c6e88d:a29aaa6e:da54579d:d1e75214:1365fb0c:714946eb:bf0113e8:3ecfb1de"
+#define BUILD_DATE "20241204"
+#define BUILD_TIME "125205"
+#define REPO_INFO "afdbce27:2835831b:1118763f:08be607d:44d67462:df1bbf82:827629a7:44ca8327:1de43ba9:b7bed6b1:6a3d6842:b9d39ab6:d5c6e88d:a29aaa6e:da54579d:d1e75214:1365fb0c:714946eb:bf0113e8:3ecfb1de"
 #define CLI_REVISION "1.5.13"
 #define API_REVISION "3.2.9"
 const char *sw_version = {FW_REVISION};
